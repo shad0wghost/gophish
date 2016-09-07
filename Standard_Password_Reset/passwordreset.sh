27b0769efd1ff28f0bfd8b8115c66fb5d552d15f
Brandon Sakamoto <brandonsaka007@gmail.com>
2016-09-06 19:44:37 -0700

    #mor professional

--- Standard_Password_Reset/passwordreset.sh
+++ Standard_Password_Reset/passwordreset.sh
@@ -4,7 +4,6 @@ file2=/tmp/users2
 
 #request sudo perms
 echo Please enter sudo password
-sudo echo thanks m8
 sudo echo sudo password accepted
 echo -------------------------------------------------------
 echo "BRANDON'S SUPER PASSWORD RESET SCRIPT"
@@ -13,10 +12,6 @@ echo -------------------------------------------------------
 cat /etc/passwd > $file1
 
 #Read and find REAL users in the user file
-
-
-
-
 cat $file1 | grep /bin/bash | cut -d: -f1 > $file2
 
 #cleanup
@@ -29,45 +24,43 @@ cat $file1
 #Begin to reset password via imput
 	echo "Please tell me who's password you want to rest:"
 	echo "(or type "'"all"'" to change all users passwords simultaneously)"
+
+#user input
 read usr
-
-
 
 #Check if user is on the system
 grep -Fx $usr $file1
 
 if [ $? -eq 0 ]
 	then
-		echo "Ok lets reset" '"'$usr'"'
+echo "Resetting password for" '"'$usr'"'
 		#reset the pass
 		sudo passwd $usr
-#if user types all
+		#if user types all
 elif [[ $usr = "all" ]]; then
-	#yes no thing
-
-	echo you have elected to change all of the users passwords are you sure? [yes/no]
-	read makesure
+	#Make sure of choice
+		echo You have elected to change all of the users passwords are you sure? [yes/no]
+		read makesure
 			if [ $makesure = "yes" ]
 			then
 					echo "ok what do you want EVERYONE's password to be?"
 					read passwordall
-					echo give me a sec changin teh passwords
+					echo Now changing passwords...
 
-											#all passwrod changer
+#Every user changer loop
 
-											for i in `cat $file1`; do
-						sudo yes $passwordall | sudo passwd $i
+					for i in `cat $file1`; do
+							sudo yes $passwordall | sudo passwd $i
 					done
-echo "Ok everyone's password is now" $passwordall
-
+							echo "Changed all passwords to" $passwordall
 			else
-				echo "KK fam will not change everyone's password"
-		fi
+							echo "Passwords unchanged."
+		 fi
 	else
 		#fail condition
-		echo No matching user found but lets try it anyway :D
+		echo No matching user found, attempting anyway.
 
-		echo "Ok lets reset" '"'$usr'"'
+		echo "Resetting password for" '"'$usr'"'
 		#reset the pass
 		sudo passwd $usr
 fi
